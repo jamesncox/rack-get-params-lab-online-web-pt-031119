456a69dd002c3a6ba6de830dcf8297ad4e0b56f2
James Cox <jamesncox@yahoo.com>
2019-06-04 16:28:12 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -24,7 +24,7 @@ class Application
         end
       end
     elsif req.path.match(/add/)
-      added_item = req.params[]
+      added_item = req.params["items"]
     else
       resp.write "Path Not Found"
     end
