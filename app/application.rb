962200c0dc741dd479686c5de437cc8a32917c1b
James Cox <jamesncox@yahoo.com>
2019-06-04 16:25:21 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -20,7 +20,7 @@ class Application
         resp.write "Your cart is empty"
       else 
         @@cart.each do |item|
-          resp.write 
+          resp.write "#{item}"
       end
     else
       resp.write "Path Not Found"
